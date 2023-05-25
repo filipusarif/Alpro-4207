7152e51ce19d5c7b6c754b766c19c44775c93ef5
filipusarif <filipus123.gmil@gmail.com>
2023-05-25 15:45:28 +0700

    update penjumlahan

--- Pertemuan 8/Teori/main.cpp
+++ Pertemuan 8/Teori/main.cpp
@@ -9,20 +9,55 @@
 
 using namespace std;
 
+int penjumlahan(int a, int b);
+int pengurangan(int b, int c);
+int perkalian(int c,int n);
+float pembagian(float d, int e);
+int pangkat(int e, int f);
+int fibonaci(int e);
+int faktorial(int f);
+
+int main()
+{
+    int n,m;
+    cout << "-------------------------------------------------" << endl;
+    cout << "| Program Fungsi Rekursif \t\t\t|" << endl;
+    cout << "-------------------------------------------------" << endl;
+    cout << "Masukkan Bilangan Bulat Positif ke-1 \t: ";cin >> n;
+    cout << "Masukkan Bilangan Bulat Positif ke-2 \t: ";cin >> m;
+    cout << "=================================================" << endl;
+    cout << "Penjumlahan "<< n << " + " << m << " \t\t: " << penjumlahan(n, m) << endl;
+    cout << "Pengurangan "<< n << " - " << m << " \t\t: " << pengurangan(n, m) << endl;
+    cout << "Perkalian "  << n << " * " << m << " \t\t: " << perkalian(n,m) << endl;
+    cout << "Pembagian "  << n << " / " << m << " \t\t: " << pembagian(n,m) << endl;
+    cout << "Pangkat "    << n << " ^ " << m << " \t\t\t: " << pangkat(n,m) << endl;
+    cout << "Fibonaci index ke-" << n << " \t\t: " << fibonaci(n) << endl;
+    for(int i=0; i<=n ;i++){
+        cout << fibonaci(i) << " ";
+    }
+    cout << endl;
+    cout << "Faktorial !" << n << " \t\t\t: " << faktorial(n) << endl;
+    cout << "=================================================" << endl;
+    return 0;
+}
+
 int penjumlahan(int a, int b){
-    if(b == 0)
-        return a;
+    if (a > 0)
+        return 1 + penjumlahan(a-1, b);
+    else if (b > 0)
+        return 1 + penjumlahan(a, b-1);
     else
-        return 1 + penjumlahan(a,b-1);
+        return 0;
 }
 
 int pengurangan(int b, int c){
-    if(c == 0)
-        return b;
-    else
+    if (b > 0)
+        return pengurangan(b-1, c) +1;
+    else if (c > 0)
         return pengurangan(b, c-1) -1;
+    else
+        return 0;
 }
-
 
 int perkalian(int c,int n){
     if (n == 0)
@@ -33,9 +68,9 @@ int perkalian(int c,int n){
         return (-c) + perkalian(c, n+1);
 }
 
-int pembagian(int d, int e){
+float pembagian(float d, int e){
     if( d < e )
-        return 0;
+        return d / e;
     else
         return 1 + pembagian(d-e, e);
 }
@@ -64,26 +99,3 @@ int faktorial(int f){
         return f * faktorial(f-1);
 }
 
-int main()
-{
-    int n,m;
-    cout << "-------------------------------------------------" << endl;
-    cout << "| Program Fungsi Rekursif \t\t\t|" << endl;
-    cout << "-------------------------------------------------" << endl;
-    cout << "Masukkan Bilangan ke-1 \t\t: ";cin >> n;
-    cout << "Masukkan Bilangan ke-2 \t\t: ";cin >> m;
-    cout << "=================================================" << endl;
-    cout << "Penjumlahan "<< n << " + " << m << " \t\t: " << penjumlahan(n, m) << endl;
-    cout << "Pengurangan "<< n << " - " << m << " \t\t: " << pengurangan(n, m) << endl;
-    cout << "Perkalian "  << n << " * " << m << " \t\t: " << perkalian(n,m) << endl;
-    cout << "Pembagian "  << n << " / " << m << " \t\t: " << pembagian(n,m) << endl;
-    cout << "Pangkat "    << n << " ^ " << m << " \t\t\t: " << pangkat(n,m) << endl;
-    cout << "Fibonaci index ke-" << n << " \t\t: " << fibonaci(n) << endl;
-    for(int i=0; i<=n ;i++){
-        cout << fibonaci(i) << " ";
-    }
-    cout << endl;
-    cout << "Faktorial !" << n << " \t\t\t: " << faktorial(n) << endl;
-    cout << "=================================================" << endl;
-    return 0;
-}
